d3fc97e4d48e7c64141a03c28d74d410f3d9599c
Łukasz Urbański <lukasz.urbanski@student.uj.edu.pl>
2021-03-17 01:58:57 +0100

    Add common error messages

---
+++ api/src/errors/index.ts
@@ -0,0 +1,27 @@
+abstract class HttpError extends Error {
+    public status: number;
+}
+
+export class BadRequest extends HttpError {
+    constructor(message = "Bad Request") {
+        super(message);
+
+        this.status = 400;
+    }
+}
+
+export class ServiceUnavailable extends HttpError {
+    constructor(message = "Service Unavailable") {
+        super(message);
+
+        this.status = 503;
+    }
+}
+
+export class NotFound extends HttpError {
+    constructor(message = "Not Found") {
+        super(message);
+
+        this.status = 404;
+    }
+}
